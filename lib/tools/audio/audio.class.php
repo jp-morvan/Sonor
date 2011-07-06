532165d60142181c9cfa3de11735aa2b20f09362
Morvan Jean-Philippe <jeanphilippe.morvan@gmail.com>
2011-07-06 16:30:50 +0200

    update audio class and refactoring of tags array

--- lib/tools/audio/audio.class.php
+++ lib/tools/audio/audio.class.php
@@ -13,7 +13,7 @@ class audio extends getID3
           $_id3v1_tags = array(),
           $_lyrics3_tags = array(),
           $_ape_tags = array(),
-          $_id3v2_fields = array('title', 'artist', 'album', 'track_number'),
+          $_id3v2_fields = array('title', 'artist', 'album', 'track_number', 'band'),
           $_id3v1_fields = array('title', 'artist', 'album', 'track'),
           $_lyrics3_fields = array('title', 'artist', 'album', 'track'),
           $_ape_fields = array('title', 'artist', 'album', 'track'),
@@ -29,7 +29,15 @@ class audio extends getID3
       $this->setTags();
     }
   }
-
+  
+  public function hasTypeOfTag($type)
+  {
+    if($this->hasTags())
+    {
+      return array_key_exists($type, $this->getTags());
+    }
+    return false;
+  }
   
   public function hasTags()
   {
@@ -53,26 +61,60 @@ class audio extends getID3
   {
     $tags = $this->{'_'.$tag_type.'_tags'};
     $tmp = array();
-    foreach($this->{'_'.$tag_type.'_fields'} as $field)
+    // ID3V2
+    if($tag_type == 'id3v2')
     {
-      if(isset($tags[$field]))
+      if(isset($tags['title']))
       {
-        $tmp[($field == "track_number") ? 'track': $field] = $tags[$field][0];
+        $tmp['title'] = $this->_getFieldValue('id3v2', 'title');
       }
+      if(isset($tags['artist']) || isset($tags['band']))
+      {
+        $tmp['artist'] = (isset($tags['artist']) ? $this->_getFieldValue('id3v2', 'artist'): $this->_getFieldValue('id3v2', 'band'));
+      }
+      if(isset($tags['album']))
+      {
+        $tmp['album'] = $this->_getFieldValue('id3v2', 'album');
+      }
+      if(isset($tags['track_number']))
+      {
+        $tmp['track'] = $this->_getFieldValue('id3v2', 'track_number');
+      }
+    }
+    // ID3V1
+    if($tag_type == 'id3v1')
+    {
+      if(isset($tags['title']))
+      {
+        $tmp['title'] = $this->_getFieldValue('id3v1', 'title');
+      }
+      if(isset($tags['artist']))
+      {
+        $tmp['artist'] = $this->_getFieldValue('id3v1', 'artist');
+      }
+      if(isset($tags['album']))
+      {
+        $tmp['album'] = $this->_getFieldValue('id3v1', 'album');
+      }
+      if(isset($tags['track']))
+      {
+        $tmp['track'] = $this->_getFieldValue('id3v1', 'track');
+      }
+    }
+    // LYRICS3
+    if($tag_type == 'lyrics3')
+    {
+    }
+    // APE
+    if($tag_type == 'ape')
+    {
     }
     $this->{'_'.$tag_type.'_tags'} = $tmp;
   }
   
   private function _mergeTags()
   {
-    foreach($this->_tags_type as $tag_type)
-    {
-      $count = count($this->{'_'.$tag_type.'_tags'});
-      if($count == 4)
-      {
-        $this->_tags = $this->{'_'.$tag_type.'_tags'};
-      }
-    }
+    $this->_tags = array_merge($this->_ape_tags, $this->_lyrics3_tags, $this->_id3v1_tags, $this->_id3v2_tags);
   }
     
   public function getDuration() 
@@ -102,6 +144,12 @@ class audio extends getID3
       return $this->_tags[$tag];
     }
     return null;
+  }
+  
+  private function _getFieldValue($tag_type, $field)
+  {
+    $tag = $this->{'_'.$tag_type.'_tags'};
+    return $tag[$field][0];
   }
 }
 
